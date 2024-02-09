33f1fdd12fff0219aef20ce6bd066e4484dc98e1
kelsey williams <kelseywilliams1200@gmail.com>
2024-02-09 10:46:40 +0000

    Working SSL

--- web/index.php
+++ web/index.php
@@ -2,4 +2,4 @@ Under construction.
 
 Here's a cat picture meanwhile
 
-<img src="https://i.imgur.com/AuhIXtw.jpeg">
+<img src="https://static.wikia.nocookie.net/cursed-commercials/images/7/75/K-fee_Zombie.png/revision/latest/scale-to-width-down/810?cb=20210730212507">
