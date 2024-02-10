adbdc2a26b135806724a53ed3b168c631abdba8f
Kelsey Williams <kelseywilliams1200@gmail.com>
2024-02-10 15:21:20 -0800

    funni

--- web/site/index.php
+++ web/site/index.php
@@ -1,3 +1,6 @@
 <?php
-    header('Location: unnamed.png');
+    //header('Location: unnamed.png');
 ?>
+Hey you better chiggity check yourself before you wreckity wreck yourself.
+
+<script>setTimeout(()=>{window.location.href="unnamed.png"},1000*5);</script>
