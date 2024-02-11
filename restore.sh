5b0a9fb90329caa7302670b887847a2de00f09a7
Kelsey Williams <kelseywilliams1200@gmail.com>
2024-02-10 18:50:32 -0800

    Db works

--- restore.sh
+++ restore.sh
@@ -1,4 +1,4 @@
 #!/bin/bash/
 # restore db from docker backup_vol volume. Stores last resort backup out of cwd
-cp /var/lib/docker/kelseywilliamsco_backup_vol/backup.sql web/database/migrations/seed.sql
+cp /var/lib/docker/kelseywilliamsco_backup_vol/backup.sql web/database/migrations/backup.sql
 cp /var/lib/docker/kelseywilliamsco_backup_vol/backup.sql /var/lib/kelseywilliamsco_backup_vol.sql
