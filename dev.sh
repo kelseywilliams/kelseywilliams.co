1e1754161763ec21e4247ab7e2b7ee1ae641ef6b
Kelsey Williams <kelseywilliams1200@gmail.com>
2024-03-12 23:45:54 -0700

    Proxying traffic

--- dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# docker-compose substitute for Unix environments
-# purges all containers and images and restarts
-# just call (bash) ./dc 
-
-docker-compose down
-docker container prune -f
-docker image prune -af
-docker-compose -f docker-compose.yml up
--- runl.sh
+++ runl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# docker-compose substitute for Unix environments
-# purges all containers and images and restarts
-# just call (bash) ./dc 
+# Launcher for bourne again shell
+#     * purges all containers and
+#       images and restarts
 docker stop $(docker ps -a -q)
 docker container prune -f
 docker image prune -af
