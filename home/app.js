40551330923f2ebe47cdeef2027bf930be517bba
Kelsey Williams <kelseywilliams1200@gmail.com>
2024-09-21 23:06:45 -0700

    Added node env setting

--- home/app.js
+++ home/app.js
@@ -1,6 +1,10 @@
 const express = require("express")
+const http = require("http");
+const https = require("https");
+const fs = require("fs");
 const app = express()
 const port = 3000
+
 
 app.use(express.static("public"));
 app.get("/", (req, res) => {
@@ -8,5 +12,18 @@ app.get("/", (req, res) => {
     res.sendStatusCode(200);
 })
 
-app.listen(3000);
+var server;
+if (process.env.NODE_ENV === "production") {
+    var key = fs.readFileSync("/etc/ssl/certs/kelseywilliams.co.key");
+    var cert = fs.readFileSync("/etc/ssl/certs/kelseywilliams.co.crt");
+    var options = {
+        key: key,
+        cert: cert
+    };
+
+    var server = https.createServer(options, app);
+    server.listen(3000);
+} else server = http.createServer(app);
+server.listen(3000);
+
 console.log('Express started on port 3000');
