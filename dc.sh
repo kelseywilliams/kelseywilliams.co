57553126aa043fe184f30ff996d8c85c69966832
Kelsey Williams <kelseywilliams1200@gmail.com>
2025-01-19 00:11:21 -0800

    Removed dc as dockerkiller is used for cnt mngmnt

--- dc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# docker-compose substitute for Unix environments
-# purges all containers and images and restarts
-# just call
-docker-compose down
-docker container prune -f
-docker image prune -af
-docker-compose up
